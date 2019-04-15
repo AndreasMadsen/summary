f43530807339488e457e3c2c25997de94d1d1979
Samuel Marks <samuelmarks@gmail.com>
2019-04-15 23:45:38 +1000

    TypeScript rewrite; replace tap with mocha & assert; minor other improvements

--- summary.ts
+++ summary.ts
@@ -1,20 +1,42 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+type NumberCollections = number[] | Int8Array | Uint8Array | Int16Array | Uint16Array |
+    Int32Array | Uint32Array | Float32Array | Float64Array;
+
 const array_types = Object.freeze([
     Array, Int8Array, Uint8Array, Int16Array, Uint16Array,
     Int32Array, Uint32Array, Float32Array, Float64Array
 ]);
-class Summary {
-    constructor(_data, sorted) {
-        this._data = _data;
-        if (array_types.indexOf(_data.constructor) === -1) {
+
+export class Summary {
+    _sorted: NumberCollections | undefined;
+    _length: number;
+    _cache_sum: number | undefined;
+    _cache_mode: number | undefined;
+    _cache_mean: number | undefined;
+    _cache_quartiles: {[q: number]: number;};
+    _cache_constiance: number | undefined;
+    _cache_sd: number | undefined;
+    _cache_max: number | undefined;
+    _cache_min: number | undefined;
+    _cache_variance: number | undefined;
+
+    constructor(public _data: NumberCollections, sorted?: boolean) {
+        this.init(_data, sorted);
+    }
+
+    static _(_data: NumberCollections, sorted?: boolean): Summary {
+        return new Summary(_data, sorted);
+    }
+
+    public init(_data: NumberCollections, sorted?: boolean) {
+        if (array_types.indexOf(_data.constructor as any) === -1) {
             throw TypeError('data must be an array');
-        }
-        else if (!_data.length) {
+        } else if (!_data.length) {
             throw TypeError('data must exceed length of 0');
         }
+
         this._sorted = sorted ? _data : undefined;
         this._length = _data.length;
+
         this._cache_sum = undefined;
         this._cache_mode = undefined;
         this._cache_mean = undefined;
@@ -25,97 +47,110 @@ class Summary {
         this._cache_min = undefined;
         this._cache_variance = undefined;
     }
-    static _(_data, sorted) {
-        return new Summary(_data, sorted);
-    }
+
     //
-    // Not all values are in lazy calculated since that wouldn't do any good
-    //
+// Not all values are in lazy calculated since that wouldn't do any good
+//
     data() {
         return this._data;
     }
+
     sort() {
         if (this._sorted == null) {
-            this._sorted = this._data.slice(0).sort(function (a, b) { return a - b; });
+            this._sorted = this._data.slice(0).sort(function(a, b) { return a - b; });
         }
+
         return this._sorted;
     }
+
     size() {
         return this._length;
     }
+
     //
     // Always lazy calculated functions
     //
+
     sum() {
         if (this._cache_sum == null) {
             let sum = 0;
-            for (let i = 0; i < this._length; i++)
-                sum += this._data[i];
+            for (let i = 0; i < this._length; i++) sum += this._data[i];
             this._cache_sum = sum;
         }
+
         return this._cache_sum;
     }
+
     mode() {
         if (this._cache_mode == null) {
             const data = this.sort();
+
             let modeValue = NaN;
             let modeCount = 0;
             let currValue = data[0];
             let currCount = 1;
+
             // Count the amount of repeat and update mode constiables
             for (let i = 1; i < this._length; i++) {
                 if (data[i] === currValue) {
                     currCount += 1;
-                }
-                else {
+                } else {
                     if (currCount >= modeCount) {
                         modeCount = currCount;
                         modeValue = currValue;
                     }
+
                     currValue = data[i];
                     currCount = 1;
                 }
             }
+
             // Check the last count
             if (currCount >= modeCount) {
                 modeCount = currCount;
                 modeValue = currValue;
             }
+
             this._cache_mode = modeValue;
         }
+
         return this._cache_mode;
     }
-    mean() {
+
+    mean(): number {
         if (this._cache_mean == null) {
             this._cache_mean = this.sum() / this._length;
         }
-        return this._cache_mean;
+
+        return this._cache_mean as number;
     }
-    quartile(prob) {
+
+    quartile(prob: number) {
         if (!this._cache_quartiles.hasOwnProperty(prob)) {
             const data = this.sort();
             const product = prob * this.size();
             const ceil = Math.ceil(product);
+
             if (ceil === product) {
                 if (ceil === 0) {
                     this._cache_quartiles[prob] = data[0];
-                }
-                else if (ceil === data.length) {
+                } else if (ceil === data.length) {
                     this._cache_quartiles[prob] = data[data.length - 1];
-                }
-                else {
+                } else {
                     this._cache_quartiles[prob] = (data[ceil - 1] + data[ceil]) / 2;
                 }
-            }
-            else {
+            } else {
                 this._cache_quartiles[prob] = data[ceil - 1];
             }
         }
+
         return this._cache_quartiles[prob];
     }
+
     median() {
         return this.quartile(0.5);
     }
+
     constiance() {
         if (this._cache_constiance == null) {
             const mean = this.mean();
@@ -123,48 +158,60 @@ class Summary {
             for (let i = 0; i < this._length; i++) {
                 sqsum += (this._data[i] - mean) * (this._data[i] - mean);
             }
+
             this._cache_constiance = sqsum / (this._length - 1);
         }
+
         return this._cache_constiance;
     }
+
     sd() {
         if (this._cache_sd == null) {
-            this._cache_sd = Math.sqrt(this.constiance());
+            this._cache_sd = Math.sqrt(this.constiance() as number);
         }
+
         return this._cache_sd;
     }
+
     max() {
         if (this._cache_max == null) {
             this._cache_max = this.sort()[this._length - 1];
         }
+
         return this._cache_max;
     }
+
     min() {
         if (this._cache_min == null) {
             this._cache_min = this.sort()[0];
         }
+
         return this._cache_min;
     }
-    variance() {
+
+    variance(): number {
         if (this._cache_variance == null) {
             const mean = this.mean();
             let sqsum = 0;
             for (let i = 0; i < this._length; i++) {
                 sqsum += (this._data[i] - mean) * (this._data[i] - mean);
             }
+
             this._cache_variance = sqsum / (this._length - 1);
         }
-        return this._cache_variance;
+        return this._cache_variance as number;
     }
 }
-exports.Summary = Summary;
-exports.summary = function (_data, sorted) {
+
+interface SummaryConstructor {
+    new(_data: NumberCollections, sorted?: boolean): Summary;  // newable
+    (_data: NumberCollections, sorted?: boolean): Summary; // callable
+}
+
+export const summary: SummaryConstructor = function(this: Summary | void, _data: NumberCollections, sorted?: boolean) {
     if (!(this instanceof Summary)) {
         return new Summary(_data, sorted);
+    } else {
+        this!.init(_data, sorted);
     }
-    else {
-        this._data = _data;
-        this._sorted = sorted ? _data : undefined;
-        this._length = _data.length;
-    }
-};
+} as SummaryConstructor;
--- test.ts
+++ test.ts
@@ -1,53 +1,94 @@
-"use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const assert = __importStar(require("assert"));
-const summary_1 = require("./summary");
+import * as assert from 'assert';
+
+import { summary } from './summary';
+
 describe('summary tests', () => {
     it('testing data method', (done) => {
-        assert.deepStrictEqual(summary_1.summary([2, 3, 7, -2, 0, 1, 1]).data(), [2, 3, 7, -2, 0, 1, 1]);
+        assert.deepStrictEqual(
+            summary([2, 3, 7, -2, 0, 1, 1]).data(),
+            [2, 3, 7, -2, 0, 1, 1]
+        );
+
         done();
     });
+
     it('testing sort method', (done) => {
         const data = [2, 3, 7, -2, 0, 1, 1];
-        assert.deepStrictEqual(summary_1.summary(data, true).sort(), [2, 3, 7, -2, 0, 1, 1]);
+
+        assert.deepStrictEqual(
+            summary(data, true).sort(),
+            [2, 3, 7, -2, 0, 1, 1]
+        );
         assert.deepStrictEqual(data, [2, 3, 7, -2, 0, 1, 1], 'data shouldn\'t mutate');
-        assert.deepStrictEqual(summary_1.summary(data, false).sort(), [-2, 0, 1, 1, 2, 3, 7]);
+
+        assert.deepStrictEqual(
+            summary(data, false).sort(),
+            [-2, 0, 1, 1, 2, 3, 7]
+        );
         assert.deepStrictEqual(data, [2, 3, 7, -2, 0, 1, 1], 'data shouldn\'t mutate');
-        assert.deepStrictEqual(summary_1.summary(data).sort(), [-2, 0, 1, 1, 2, 3, 7]);
+
+        assert.deepStrictEqual(
+            summary(data).sort(),
+            [-2, 0, 1, 1, 2, 3, 7]
+        );
         assert.deepStrictEqual(data, [2, 3, 7, -2, 0, 1, 1], 'data shouldn\'t mutate');
+
         done();
     });
+
     it('testing size method', (done) => {
-        assert.deepStrictEqual(summary_1.summary([1, 10]).size(), 2);
+        assert.deepStrictEqual(
+            summary([1, 10]).size(),
+            2
+        );
+
         done();
     });
+
     it('testing sum method', (done) => {
-        assert.deepStrictEqual(summary_1.summary([1, 10]).sum(), 11);
+        assert.deepStrictEqual(
+            summary([1, 10]).sum(),
+            11
+        );
+
         done();
     });
+
     it('testing mode method', (done) => {
-        assert.deepStrictEqual(summary_1.summary([10, 11, 12, 11, 12, 7, 12]).mode(), 12);
-        assert.deepStrictEqual(summary_1.summary([10, 13, 12, 13, 12, 13, 12]).mode(), 13);
-        assert.deepStrictEqual(summary_1.summary([1, 2, 3]).mode(), 3);
+        assert.deepStrictEqual(
+            summary([10, 11, 12, 11, 12, 7, 12]).mode(),
+            12
+        );
+
+        assert.deepStrictEqual(
+            summary([10, 13, 12, 13, 12, 13, 12]).mode(),
+            13
+        );
+
+        assert.deepStrictEqual(
+            summary([1, 2, 3]).mode(),
+            3
+        );
+
         done();
     });
+
     it('testing mean method', (done) => {
-        assert.deepStrictEqual(summary_1.summary([2, 4]).mean(), 3);
+        assert.deepStrictEqual(
+            summary([2, 4]).mean(),
+            3
+        );
+
         done();
     });
+
     it('testing quartile method', (done) => {
-        var data = summary_1.summary([
+        var data = summary([
             2, 27, 10, 29, 16, 8, 5, 19, 2, 2, 18, 28,
             7, 28, 28, 25, 19, 14, 18, 21, 25, 29, 7,
             3, 21, 3, 24, 18, 12, 25
         ]);
+
         assert.deepStrictEqual(data.quartile(0.00), 2.00);
         assert.deepStrictEqual(data.quartile(0.13), 3.00);
         assert.deepStrictEqual(data.quartile(0.26), 7.00);
@@ -57,35 +98,49 @@ describe('summary tests', () => {
         assert.deepStrictEqual(data.quartile(0.78), 25.0);
         assert.deepStrictEqual(data.quartile(0.91), 28.0);
         assert.deepStrictEqual(data.quartile(1.00), 29.0);
+
         done();
     });
+
     it('testing median method', (done) => {
-        var data = summary_1.summary([
+        var data = summary([
             2, 27, 10, 29, 16, 8, 5, 19, 2, 2, 18, 28,
             7, 28, 28, 25, 19, 14, 18, 21, 25, 29, 7,
             3, 21, 3, 24, 18, 12, 25
         ]);
+
         assert.deepStrictEqual(data.median(), 18);
+
         done();
     });
+
     it('testing variance method', (done) => {
-        assert.deepStrictEqual(summary_1.summary([-2, -1, 0, 1, 2]).variance(), 2.5);
+        assert.deepStrictEqual(summary([-2, -1, 0, 1, 2]).variance(), 2.5);
+
         done();
     });
+
     it('testing sd method', (done) => {
-        assert.deepStrictEqual(summary_1.summary([-2, -1, 0, 1, 2]).sd(), Math.sqrt(2.5));
+        assert.deepStrictEqual(summary([-2, -1, 0, 1, 2]).sd(), Math.sqrt(2.5));
+
         done();
     });
+
     it('testing max method', (done) => {
-        assert.deepStrictEqual(summary_1.summary([6, 10, 2, 5]).max(), 10);
+        assert.deepStrictEqual(summary([6, 10, 2, 5]).max(), 10);
+
         done();
     });
+
     it('testing min method', (done) => {
-        assert.deepStrictEqual(summary_1.summary([6, 10, 2, 5]).min(), 2);
+        assert.deepStrictEqual(summary([6, 10, 2, 5]).min(), 2);
+
         done();
     });
+
     it('typed array', (done) => {
-        assert.deepStrictEqual(summary_1.summary(new Int8Array([2, 4])).mean(), 3);
+        assert.deepStrictEqual(summary(new Int8Array([2, 4])).mean(), 3);
+
         done();
     });
 });
