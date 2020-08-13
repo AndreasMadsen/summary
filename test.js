2534fe9720cae235c518782e403822abb6be5162
Andreas Madsen <amwebdk@gmail.com>
2020-08-13 14:37:20 +0200

    test all branches

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ test('testing data method', function (t) {
 });
 
 test('testing sort method', function (t) {
-  const data = [2, 3, 7, -2, 0, 1, 1]
+  const data = [2, 3, 7, -2, 0, 1, 1];
 
   t.deepEqual(
     summary(data, true).sort(),
@@ -35,6 +35,15 @@ test('testing sort method', function (t) {
   t.end();
 });
 
+test('testing sort method when cached', function (t) {
+  const s = summary([2, 3, 7, -2, 0, 1, 1]);
+
+  t.deepEqual(s.sort(), [-2, 0, 1, 1, 2, 3, 7]);
+  t.deepEqual(s.sort(), [-2, 0, 1, 1, 2, 3, 7]);
+
+  t.end();
+});
+
 test('testing size method', function (t) {
   t.equal(
     summary([1, 10]).size(),
@@ -53,6 +62,13 @@ test('testing sum method', function (t) {
   t.end();
 });
 
+test('testing sum method when cached', function (t) {
+  const s = summary([1, 10]);
+  t.equal(s.sum(), 11);
+  t.equal(s.sum(), 11);
+  t.end();
+});
+
 test('testing mode method', function (t) {
   t.equal(
     summary([10, 11, 12, 11, 12, 7, 12]).mode(),
@@ -65,10 +81,32 @@ test('testing mode method', function (t) {
   );
 
   t.equal(
+    summary([1, 2, 2, 3, 3, 3, 4]).mode(),
+    3
+  );
+
+  t.equal(
+    summary([1, 2, 2, 3, 3, 4]).mode(),
+    3
+  );
+
+  t.equal(
+    summary([1, 2, 2, 2, 3, 3, 4]).mode(),
+    2
+  );
+
+  t.equal(
     summary([1, 2, 3]).mode(),
     3
   );
 
+  t.end();
+});
+
+test('testing mode method when cached', function (t) {
+  const s = summary([1, 2, 3]);
+  t.equal(s.mode(), 3);
+  t.equal(s.mode(), 3);
   t.end();
 });
 
@@ -81,6 +119,13 @@ test('testing mean method', function (t) {
   t.end();
 });
 
+test('testing mean method when cached', function (t) {
+  const s = summary([2, 4]);
+  t.equal(s.mean(), 3);
+  t.equal(s.mean(), 3);
+  t.end();
+});
+
 test('testing quartile method', function (t) {
   var data = summary([
     2, 27, 10, 29, 16, 8, 5, 19, 2, 2, 18,28,
@@ -88,6 +133,17 @@ test('testing quartile method', function (t) {
     3, 21, 3, 24, 18, 12, 25
   ]);
 
+  t.equal(data.quartile(0.00), 2.00);
+  t.equal(data.quartile(0.13), 3.00);
+  t.equal(data.quartile(0.26), 7.00);
+  t.equal(data.quartile(0.39), 14.0);
+  t.equal(data.quartile(0.52), 18.0);
+  t.equal(data.quartile(0.65), 21.0);
+  t.equal(data.quartile(0.78), 25.0);
+  t.equal(data.quartile(0.91), 28.0);
+  t.equal(data.quartile(1.00), 29.0);
+
+  // when cached
   t.equal(data.quartile(0.00), 2.00);
   t.equal(data.quartile(0.13), 3.00);
   t.equal(data.quartile(0.26), 7.00);
@@ -109,9 +165,12 @@ test('testing median method', function (t) {
   ]);
 
   t.equal(data.median(), 18);
+  // when cached
+  t.equal(data.median(), 18);
 
   t.end();
 });
+
 
 test('testing variance method', function (t) {
   t.equal(summary([-2, -1, 0, 1, 2]).variance(), 2.5);
@@ -119,8 +178,24 @@ test('testing variance method', function (t) {
   t.end();
 });
 
+
+test('testing variance method when cached', function (t) {
+  const s = summary([-2, -1, 0, 1, 2]);
+  t.equal(s.variance(), 2.5);
+  t.equal(s.variance(), 2.5);
+  t.end();
+});
+
 test('testing sd method', function (t) {
   t.equal(summary([-2, -1, 0, 1, 2]).sd(), Math.sqrt(2.5));
+
+  t.end();
+});
+
+test('testing sd method when cached', function (t) {
+  const s = summary([-2, -1, 0, 1, 2]);
+  t.equal(s.sd(), Math.sqrt(2.5));
+  t.equal(s.sd(), Math.sqrt(2.5));
 
   t.end();
 });
@@ -131,14 +206,36 @@ test('testing max method', function (t) {
   t.end();
 });
 
+test('testing max method when cached', function (t) {
+  const s = summary([6, 10, 2, 5]);
+  t.equal(s.max(), 10);
+  t.equal(s.max(), 10);
+
+  t.end();
+});
+
 test('testing min method', function (t) {
   t.equal(summary([6, 10, 2, 5]).min(), 2);
 
   t.end();
 });
 
+test('testing min method when cached', function (t) {
+  const s = summary([6, 10, 2, 5]);
+  t.equal(s.min(), 2);
+  t.equal(s.min(), 2);
+
+  t.end();
+});
+
 test('typed array', function (t) {
   t.equal(summary(new Int8Array([2, 4])).mean(), 3);
+
+  t.end();
+});
+
+test('not array', function (t) {
+  t.throws(() => summary({ length: 0 }), TypeError);
 
   t.end();
 });
